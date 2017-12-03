7b9b73d9a03b3b3918eb4a2324096d75c49c46cf
Rachel Lim <rachel.y.lim@gmail.com>
2017-12-02 22:37:09 -0500

    testing dummy page.

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import styled, { css } from 'react-emotion';
+
+const Wrapper = styled('div')`
+  display: flex;
+  height: 100%;
+`;
+
+const NavBar = styled('div')`
+  float: left;
+  width: 280px;
+  height: 100%;
+`;
+
+const MainContent = styled('div')`
+  width: 100%;
+  height: 100%;
+`;
+
+const ContentImg = styled('div')`
+  background-image: url('imgs/mchale.jpeg');
+  width: 50%;
+  height: 50%;
+`;
+
 class App extends React.Component {
    render() {
       return (
-         <div>
-            Hello World ReactJS!!!
-         </div>
+        <Wrapper>
+          <NavBar>
+            This is a test navbar.
+          </NavBar>
+          <MainContent>
+            <p>This is text. This is text.This is text.This is text.This is text.This is text.</p>
+            <ContentImg />
+          </MainContent>
+        </Wrapper>
       );
    }
 }
